7b3ec2b1170cccf35f9ae5b69f949afbf63c3fb5
Pranshu <pranshgupta54@gmail.com>
2022-11-13 15:39:04 +0530

    .

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const homeContent = "OKHac habitasse platea dictumst vestibulum rhoncus est pell
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretim quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const eventsContent = "";
+const registerContent = "Registration krna nahi aata"
 
 const app = express();
 
@@ -19,7 +20,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-urlReq = ['/about', '/contact', '/events']
+urlReq = ['/about', '/contact', '/events', '/register']
 
 app.get(urlReq, function(req,res){
   const str = req.path;
@@ -44,7 +45,7 @@ app.post("/compose", function(req,res){
     content: req.body.postBody
   };
   posts.push(post);
-  // console.log(posts);
+  console.log(posts);
   res.redirect("/");
 })
 
